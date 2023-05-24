5607d838580f48ba0ec6e6338fe7c16e36f1cae0
Rohith Raju <rohith.raju@ecarxgroup.com>
2023-05-24 13:15:30 +0200

    Adding append
    Issue: NONE

--- redirect-to-file.sh
+++ redirect-to-file.sh
@@ -1,4 +1,8 @@
 #/bin/bash
-
-
-echo "hello world !!" > output.info
+## here we have the redirect to file
+echo "hello world !!" > output.info 
+## Here we are giving as shell output 
+#but this replaces the text every new time
+cat > output.info
+#Here we are appending to it
+cat >> output.info
