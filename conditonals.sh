259e6210c594ecc6bc489f794e1f733fb233d3b4
Rohith Raju <rohith.raju@ecarxgroup.com>
2023-05-24 13:42:23 +0200

    Adding conditional operations to the bash script
    Issue: NONE

--- conditonals.sh
+++ conditonals.sh
@@ -1,9 +1,21 @@
 #! /bin/bash
 
 read count
+read attendance 
 if [ $count -eq 10 ]
 then 
     echo "Hello its correct !!!"
 else 
     echo "Its not correct !!!"
+fi
+
+
+if (($attendance < 70))
+then 
+    echo "You need to pay fine"
+elif ((attendance > 70))
+then 
+    echo "You dont need to pay" 
+else 
+    echo "you are not okay to move forward"
 fi
